2a56d760474f05beff886cb20b4282ff7d3b77c6
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-09-23 16:44:11 -0300

    Activity and explication of constant

--- 10-tenty_day/10.06 - activity.py
+++ 10-tenty_day/10.06 - activity.py
@@ -1,4 +1,10 @@
 import os
 os.system("cls")
 
-# for i in range(2):
+print("Mostrando os números pares entre 1 e 10")
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(f"{i} é par.")
+    else:
+        print(f"{i} é impar")
+print("FIM")
